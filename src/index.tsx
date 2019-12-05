64bcc2de0830c5f480f91831807092e9e7ac20de
Bakhrom <alex960126@gmail.com>
2019-12-05 23:31:27 +0500

    remove rotation

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,24 @@
-import * as React from 'react'
-import styled, {keyframes, css} from 'styled-components'
+import * as React from "react";
+import styled, { keyframes, css } from "styled-components";
 
 const spin = keyframes`
   to {
     transform: rotate(1turn);
   }
-`
-const spinParams = '22s linear infinite'
+`;
+const spinParams = "12s linear infinite";
 
 const Shape = styled.div`
   transition: border-radius 1s ease-out;
   transform-origin: center;
   overflow: hidden;
-  ${(props:{size: string}) => props.size && css`
-    width: ${props.size};
-    height: ${props.size};
-  `}
-  ${(props:any) => css`
+  ${(props: { size: string }) =>
+    props.size &&
+    css`
+      width: ${props.size};
+      height: ${props.size};
+    `}
+  ${(props: any) => css`
     border-radius: ${props.radius1};
     animation: ${keyframes`
       0% {
@@ -25,10 +27,10 @@ const Shape = styled.div`
       100% {
         border-radius: ${props.radius2};
       }
-    `} ${Math.random()*10 |0 + 10}s ease-in-out infinite both alternate,
-  ${spin} ${spinParams};
+    `} ${(Math.random() * 10) | (0 + 10)}s ease-in-out infinite both alternate,
+      ${spin} ${spinParams};
   `}
-`
+`;
 
 const ShapeContent = styled.div`
   animation: ${spin} ${spinParams} reverse;
@@ -38,29 +40,33 @@ const ShapeContent = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`
+`;
 const Image = styled.img`
   transform: scale(1.25);
   width: 100%;
   height: 100%;
   object-fit: cover;
-`
-const randomRadiuses = (x:number) =>  new Array(x).fill(1).map(() => radiuses[Math.random() * radiuses.length | 0]).join(' ')
-const radiuses = ['70%','60%', '50%', '40%', '55%', '45%']
-const makeRadius1 = () => randomRadiuses(4) + ' / ' + randomRadiuses(4)
-const makeRadius2 = () => randomRadiuses(2)
+`;
+const randomRadiuses = (x: number) =>
+  new Array(x)
+    .fill(1)
+    .map(() => radiuses[(Math.random() * radiuses.length) | 0])
+    .join(" ");
+const radiuses = ["70%", "60%", "50%", "40%", "55%", "45%"];
+const makeRadius1 = () => randomRadiuses(4) + " / " + randomRadiuses(4);
+const makeRadius2 = () => randomRadiuses(2);
 
 export class Blob extends React.PureComponent<any> {
-  render () {
-    let {src, children, size, ...props} = this.props
-    let radius1 =  makeRadius1()
-    let radius2 = makeRadius2()
+  render() {
+    let { src, children, size, ...props } = this.props;
+    let radius1 = makeRadius1();
+    let radius2 = makeRadius2();
     return (
       <Shape size={size} radius1={radius1} radius2={radius2} {...props}>
         <ShapeContent>
           {src ? <Image src={src}></Image> : children}
         </ShapeContent>
       </Shape>
-    )
+    );
   }
 }
